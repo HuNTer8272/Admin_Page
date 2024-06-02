00c47afdad5de6eb72731fae43e4a478f898d2ad
HuNTer8272 <111863590+HuNTer8272@users.noreply.github.com>
2024-06-02 16:47:24 +0530

    first commit

--- middleware.js
+++ middleware.js
@@ -4,10 +4,12 @@ export default function middleware(req){
     let verify = req.cookies.get('loggedin');
     let url = req.url
 
+    // only if the the user is logged in show the dashboard
     if(!verify && url.includes('/dashboard')){
         return NextResponse.redirect("http://localhost:3000/")
     }
 
+    // if the user has logged in before than redirect to dashboard automatically 
     // if(verify && url === "http://localhost:3000/"){
     //     return NextResponse.redirect("http://localhost:3000/dashboard")
     // }
